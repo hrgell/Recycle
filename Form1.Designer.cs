5856092969ca0526000029f4ca545ccf62b28cdf
Hr Gell <71132772+hrgell@users.noreply.github.com>
2021-02-14 02:23:06 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace TreeViewTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tv1 = new System.Windows.Forms.TreeView();
+            this.Tv2 = new System.Windows.Forms.TreeView();
+            this.Tv3 = new System.Windows.Forms.TreeView();
+            this.Tv4 = new System.Windows.Forms.TreeView();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.Btn3 = new System.Windows.Forms.Button();
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Tv1
+            // 
+            this.Tv1.AllowDrop = true;
+            this.Tv1.Location = new System.Drawing.Point(42, 26);
+            this.Tv1.Name = "Tv1";
+            this.Tv1.Size = new System.Drawing.Size(156, 157);
+            this.Tv1.TabIndex = 0;
+            this.Tv1.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.Tv1_ItemDrag);
+            this.Tv1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.Tv1_AfterSelect);
+            this.Tv1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.Tv1_NodeMouseClick);
+            this.Tv1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Tv1_DragDrop);
+            this.Tv1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Tv1_DragEnter);
+            this.Tv1.DragOver += new System.Windows.Forms.DragEventHandler(this.Tv1_DragOver);
+            this.Tv1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Tv1_MouseClick);
+            this.Tv1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Tv1_MouseDown);
+            // 
+            // Tv2
+            // 
+            this.Tv2.AllowDrop = true;
+            this.Tv2.Location = new System.Drawing.Point(214, 26);
+            this.Tv2.Name = "Tv2";
+            this.Tv2.Size = new System.Drawing.Size(156, 157);
+            this.Tv2.TabIndex = 1;
+            this.Tv2.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.Tv1_ItemDrag);
+            this.Tv2.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.Tv1_AfterSelect);
+            this.Tv2.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.Tv1_NodeMouseClick);
+            this.Tv2.DragDrop += new System.Windows.Forms.DragEventHandler(this.Tv1_DragDrop);
+            this.Tv2.DragEnter += new System.Windows.Forms.DragEventHandler(this.Tv1_DragEnter);
+            this.Tv2.DragOver += new System.Windows.Forms.DragEventHandler(this.Tv1_DragOver);
+            this.Tv2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Tv1_MouseClick);
+            this.Tv2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Tv1_MouseDown);
+            // 
+            // Tv3
+            // 
+            this.Tv3.AllowDrop = true;
+            this.Tv3.Location = new System.Drawing.Point(383, 26);
+            this.Tv3.Name = "Tv3";
+            this.Tv3.Size = new System.Drawing.Size(156, 157);
+            this.Tv3.TabIndex = 2;
+            this.Tv3.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.Tv1_ItemDrag);
+            this.Tv3.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.Tv1_AfterSelect);
+            this.Tv3.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.Tv1_NodeMouseClick);
+            this.Tv3.DragDrop += new System.Windows.Forms.DragEventHandler(this.Tv1_DragDrop);
+            this.Tv3.DragEnter += new System.Windows.Forms.DragEventHandler(this.Tv1_DragEnter);
+            this.Tv3.DragOver += new System.Windows.Forms.DragEventHandler(this.Tv1_DragOver);
+            this.Tv3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Tv1_MouseClick);
+            this.Tv3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Tv1_MouseDown);
+            // 
+            // Tv4
+            // 
+            this.Tv4.AllowDrop = true;
+            this.Tv4.Location = new System.Drawing.Point(548, 26);
+            this.Tv4.Name = "Tv4";
+            this.Tv4.Size = new System.Drawing.Size(156, 157);
+            this.Tv4.TabIndex = 3;
+            this.Tv4.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.Tv1_ItemDrag);
+            this.Tv4.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.Tv1_AfterSelect);
+            this.Tv4.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.Tv1_NodeMouseClick);
+            this.Tv4.DragDrop += new System.Windows.Forms.DragEventHandler(this.Tv1_DragDrop);
+            this.Tv4.DragEnter += new System.Windows.Forms.DragEventHandler(this.Tv1_DragEnter);
+            this.Tv4.DragOver += new System.Windows.Forms.DragEventHandler(this.Tv1_DragOver);
+            this.Tv4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Tv1_MouseClick);
+            this.Tv4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Tv1_MouseDown);
+            // 
+            // Btn1
+            // 
+            this.Btn1.Location = new System.Drawing.Point(258, 443);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(75, 23);
+            this.Btn1.TabIndex = 4;
+            this.Btn1.Text = "Btn1";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // Btn2
+            // 
+            this.Btn2.Location = new System.Drawing.Point(349, 443);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(75, 23);
+            this.Btn2.TabIndex = 5;
+            this.Btn2.Text = "Btn2";
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // Btn3
+            // 
+            this.Btn3.Location = new System.Drawing.Point(441, 443);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.Size = new System.Drawing.Size(75, 23);
+            this.Btn3.TabIndex = 6;
+            this.Btn3.Text = "Btn3";
+            this.Btn3.UseVisualStyleBackColor = true;
+            this.Btn3.Click += new System.EventHandler(this.Btn3_Click);
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(42, 208);
+            this.Txt1.Multiline = true;
+            this.Txt1.Name = "Txt1";
+            this.Txt1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Txt1.Size = new System.Drawing.Size(662, 200);
+            this.Txt1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.Txt1);
+            this.Controls.Add(this.Btn3);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.Btn1);
+            this.Controls.Add(this.Tv4);
+            this.Controls.Add(this.Tv3);
+            this.Controls.Add(this.Tv2);
+            this.Controls.Add(this.Tv1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView Tv1;
+        private System.Windows.Forms.TreeView Tv2;
+        private System.Windows.Forms.TreeView Tv3;
+        private System.Windows.Forms.TreeView Tv4;
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.Button Btn3;
+        private System.Windows.Forms.TextBox Txt1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+// Add settings:
+//
+// ExpandOnDrop
+// ExpandOnDropFirst First/Last - when dropping multiple
+// InsertAfter True/False - insert before or after the row we drop on.
+// DropIntoFolder
+
+namespace TreeViewTest
+{
+    public partial class Form1 : Form
+    {
+        long mycount = 0;
+        //TODO Add these setting 
+        bool ExpandOnDrop = true; // expand the folder the items are dopped into
+        bool ExpandOnDropFirst = true; // when dropping multiple focus on first or last copied.
+        bool InsertAfter = true; // insert after the row we drop on ?
+        bool DropIntoFolder = true; // drop into folder instead of after.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Debug(string msg)
+        {
+            //Txt1.Text += msg + Environment.NewLine;
+            Txt1.AppendText(msg + Environment.NewLine);
+        }
+
+        private void FillTreeView(TreeNodeCollection nodes, string prefix)
+        {
+            nodes.Add(prefix + "1", prefix + "1");
+            nodes.Add(prefix + "2", prefix + "2");
+            var node = nodes.Add(prefix + "3", prefix + "3");
+            nodes.Add(prefix + "4", prefix + "4");
+            nodes.Add(prefix + "5", prefix + "5");
+
+            nodes = node.Nodes;
+            nodes.Add(prefix + "A", prefix + "A");
+            nodes.Add(prefix + "B", prefix + "B");
+            node = nodes.Add(prefix + "C", prefix + "C");
+            nodes.Add(prefix + "D", prefix + "D");
+
+            nodes = node.Nodes;
+            nodes.Add(prefix + "X", prefix + "X");
+            nodes.Add(prefix + "Y", prefix + "Y");
+            nodes.Add(prefix + "Z", prefix + "Z");
+        } // FillTreeView()
+
+        private void FillTreeViews()
+        {
+            FillTreeView(Tv1.Nodes, "test");
+            FillTreeView(Tv2.Nodes, "qwer");
+            FillTreeView(Tv3.Nodes, "asdf");
+            FillTreeView(Tv4.Nodes, "zxcv");
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            FillTreeViews();
+        }
+
+        // Only accepts clicks on the nodes
+        private void Tv1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            //Debug(e.Node.Text);
+        }
+
+        // Only accepts clicks on node rows
+        private void Tv1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            //Debug("NodeMouseClick " + e.Node.Text);
+            TreeView tv = (TreeView)sender;
+            TreeNode node = e.Node;
+            if (node == null && tv.Nodes.Count > 1)
+                node = tv.Nodes[tv.Nodes.Count - 1];
+            tv.SelectedNode = e.Node;
+        }
+
+
+        // Only accepts clicks on node rows
+        private void Tv1_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Tv1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //TreeView tv = (TreeView)sender;
+            //Point targetPoint = tv.PointToClient(new Point(e.X, e.Y));
+            //TreeNode node = tv.GetNodeAt(targetPoint);
+            //if (node == null && tv.Nodes.Count > 0)
+            //{
+            //    node = tv.Nodes[tv.Nodes.Count - 1];
+            //    tv.SelectedNode = node;
+            //}
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void Tv1_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            //Debug("ItemDrag");
+            if (e.Button == MouseButtons.Left)
+            {
+                //if(meta key is pressed)
+                //    DoDragDrop(e.Item, DragDropEffects.Move);
+                //else
+                DoDragDrop(e.Item, DragDropEffects.Copy);
+            }
+        }
+
+        private void Tv1_DragEnter(object sender, DragEventArgs e)
+        {
+            //Debug("DragEnter");
+            //e.Effect = DragDropEffects.Move;
+            e.Effect = e.AllowedEffect;
+        }
+
+        private void Tv1_DragOver(object sender, DragEventArgs e)
+        {
+            //Debug("DragOwer");
+            TreeView tv = (TreeView)sender;
+            Point targetPoint = tv.PointToClient(new Point(e.X, e.Y));
+            tv.SelectedNode = tv.GetNodeAt(targetPoint);
+        }
+
+
+        private void Tv1_DragDrop(object sender, DragEventArgs e)
+        {
+            //Debug("DragDrop");
+            TreeNodeCollection nodes = null;
+            TreeView tv = (TreeView)sender;
+            Point point = tv.PointToClient(new Point(e.X, e.Y));
+            TreeNode destination = tv.GetNodeAt(point);
+            if (destination == null)
+            {
+                nodes = tv.Nodes;
+                if (nodes.Count > 0)
+                    destination = nodes[nodes.Count - 1];
+            }
+            if (nodes == null && destination != null)
+            {
+                if (destination.Level == 0 && destination.Nodes.Count == 0)
+                    nodes = tv.Nodes;
+                else if (destination.Nodes.Count > 0 && DropIntoFolder)
+                    nodes = destination.Nodes;
+                else if (destination.Parent != null)
+                    nodes = destination.Parent.Nodes;
+            }
+            if (nodes == null)
+                nodes = tv.Nodes;
+
+            TreeNode source = (TreeNode)e.Data.GetData(typeof(TreeNode));
+            if (source == null)
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    int idx = (destination != null) ? destination.Index : 0;
+                    if (InsertAfter)
+                        ++idx;
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    foreach (string filename in files)
+                    {
+                        //Debug(filename);
+                        TreeNode node = nodes.Insert(idx, filename, filename);
+                        ++idx;
+                    }
+                }
+                return;
+            }
+            if (source.Equals(destination))
+                return;
+            //if (destination != null)
+            //    tv.SelectedNode = destination;
+            if (e.Effect == DragDropEffects.Move)
+            {
+                source.Remove();
+                //TreeNode node = nodes.Add(source.Text, source.Text);
+                //node.EnsureVisible();
+            }
+            else if (e.Effect == DragDropEffects.Copy)
+            {
+                TreeNode node = (TreeNode)source.Clone();
+                //int idx = nodes.Add(node);
+                //TreeNode node2 = nodes[idx];
+                int idx = 0;
+                if (destination != null && destination.Nodes.Count > 0 && DropIntoFolder)
+                {
+                    idx = destination.Nodes.Count;
+                }
+                else
+                {
+                    idx = (destination != null) ? destination.Index : 0;
+                }
+                if (InsertAfter)
+                    ++idx;
+                nodes.Insert(idx, node);
+                if(ExpandOnDrop)
+                    node.EnsureVisible();
+            }
+            if (destination != null)
+            {
+                //tv.SelectedNode = destination;
+            }
+
+        } // DragDrop()
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            Tv1.Nodes.Clear();
+            Tv2.Nodes.Clear();
+            Tv3.Nodes.Clear();
+            Tv4.Nodes.Clear();
+            FillTreeViews();
+        }
+
+        private void TestTv1(string key)
+        {
+            bool rst = Tv1.Nodes.ContainsKey(key);
+            Debug("TestTv1: " + rst.ToString());
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            Debug("mycount " + ++mycount);
+            TestTv1("test1");
+            TestTv1("testA");
+        }
+    } // class
+} // namespace
